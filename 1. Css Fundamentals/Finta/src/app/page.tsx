54764c64b970a28840941a9ea63f810d03ad68e4
Kisan-jena <kisan.k.k.jena.01@gmail.com>
2025-12-14 16:58:37 +0530

    Hero compo initialize

--- 1. Css Fundamentals/Finta/src/app/page.tsx
+++ 1. Css Fundamentals/Finta/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Container from '@/components/Container';
 import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
 
 export default function page() {
   return (
     <div className='layout'>
       <Container>
-        <Navbar/> 
+        <Navbar />
+        <Hero/>
       </Container>
     </div>
   );
